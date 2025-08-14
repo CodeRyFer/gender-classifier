37f62971768c20378edb564ac0e4a42795cb45f6
Ryan Fehr <coderry4@gmail.com>
2025-08-13 20:09:49 -0500

    Test models added

--- src/main.py
+++ src/main.py
@@ -8,3 +8,11 @@ from sklearn.neighbors import KNeighborsClassifier
 x = [[72, 180, 11], [75, 200, 12], [56, 120, 8], [54, 110, 7], [67, 145, 10], [66, 135, 9]]
 y = ['male', 'male', 'female', 'female', 'female', 'male']
 
+# Test different models
+models = {
+    'Decision Tree': tree.DecisionTreeClassifier(),
+    'Random Forest': RandomForestClassifier(),
+    'SVM': SVC(),
+    'Naive Bayes': GaussianNB(),
+    'KNN': KNeighborsClassifier(n_neighbors=3)
+}
