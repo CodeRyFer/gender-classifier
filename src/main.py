12e6a5cd65ae5050f22fb28cb18718fc3742329a
Ryan Fehr <coderry4@gmail.com>
2025-08-13 20:08:09 -0500

    Added dependencies

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+
