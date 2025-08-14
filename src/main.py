f9c07a0bf3d4ce47d073ea4635e1cf65f91a3e2a
Ryan Fehr <coderry4@gmail.com>
2025-08-13 20:09:08 -0500

    Variables with source data created

--- src/main.py
+++ src/main.py
@@ -4,3 +4,7 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 
+# [[height(inches), weight(pounds), shoe size]]
+x = [[72, 180, 11], [75, 200, 12], [56, 120, 8], [54, 110, 7], [67, 145, 10], [66, 135, 9]]
+y = ['male', 'male', 'female', 'female', 'female', 'male']
+
